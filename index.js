610242354a2a95862b8fbca302805f02d853b722
uziriahimu <uziriahimu@gmail.com>
2022-09-03 15:18:50 +0600

    add

--- index.js
+++ index.js
@@ -19,23 +19,27 @@ const displayCategories = categories => {
         loadCategory.appendChild(div)
 
     });
+
+
 }
 
 const loadNewsDetails = (id) => {
     const url = `https://openapi.programming-hero.com/api/news/category/${id}`
     fetch(url)
         .then(res => res.json())
-        .then(data => displayNewsDetails(data.data[0]))
+        .then(data => displayNewsDetails(data.data))
 
 }
 
-const displayNewsDetails = category => {
-    console.log(category);
-    const showDetail = document.getElementById('show-details');
-    const detailsdiv = document.createElement('div')
-    detailsdiv.classList.add('row')
+const displayNewsDetails = categories => {
 
-    detailsdiv.innerHTML = `
+    const showDetail = document.getElementById('show-details');
+    categories.forEach(category => {
+
+        const detailsdiv = document.createElement('div')
+        detailsdiv.classList.add('row')
+
+        detailsdiv.innerHTML = `
     <div class="col-md-4">
         <img src="${category.thumbnail_url}" class="img-fluid ms-5 mt-4 " alt="...">
         </div>
@@ -49,26 +53,48 @@ const displayNewsDetails = category => {
             <img src="${category.author.img}" class="img-fluid rounded-circle pe-2  w-25"
                 alt="">
             <div>
-                <h6 class="header-color">${category.author.name}</h6>
-                <p class="p-color ">${category.author.published_date}</p>
+                <h6 class="header-color">${category.author ? category.author.name : 'No data found'}</h6>
+                <p class="p-color ">${category.author ? category.author.published_date : 'No data found'}</p>
             </div>
         </div>
         <div class="d-flex pe-5 " >
         <div><i class="fa-regular fa-eye pe-2"></i></div>
-        <div>${category.total_view}</div>
+        <div>${category.total_view ? category.total_view : 'No data found'}</div>
         </div>
-        <div> <button class="btn btn-primary" type="submit">Details</button></div>
+        <div>
+         <button onclick="displayDetails ('${category.category_id}') " class="btn btn-primary" type="submit" data-bs-toggle="modal" data-bs-target="#showModal">Details</button>
+         </div>
     </div>
 </div>
 </div>
-                            </div>
-                        </div>
+ </div>
+   </div>
                     
-    
-
         `
-    showDetail.appendChild(detailsdiv)
+        showDetail.appendChild(detailsdiv)
+    });
+
 }
+
+
+// const loadDetails = (id) => {
+//     const url = `https://openapi.programming-hero.com/api/news/category/${id}`
+//     fetch(url)
+//         .then(res => res.json())
+//         .then(data => displayDetails(data.data[0]))
+
+// }
+// const displayDetails = cat => {
+
+//     const Details = document.getElementById('showModalLabel')
+//     Details.innerText = cat.author.name
+//     const modalBody = document.getElementById('show-details')
+//     modalBody.innerHTML = `
+
+// `
+
+// }
+// loadDetails()
 // loadNewsDetails()
 
 loadCategories()
